dc2cf48c79c5eb6297e3aae1d29f36037123b428
Anderson <Anderson@NoteQuarto>
2015-07-14 16:55:40 -0300

    Todos testes funcionais de criacao

---
+++ src/testes/testeCriarTicket.java
@@ -0,0 +1,84 @@
+package testes;
+
+import org.dbunit.Assertion;
+import org.dbunit.DBTestCase;
+import org.dbunit.PropertiesBasedJdbcDatabaseTester;
+import org.dbunit.database.DatabaseConfig;
+import org.dbunit.dataset.IDataSet;
+import org.dbunit.dataset.ITable;
+import org.dbunit.dataset.filter.DefaultColumnFilter;
+import org.dbunit.dataset.xml.FlatXmlDataSet;
+import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
+import org.dbunit.ext.h2.H2DataTypeFactory;
+import org.dbunit.operation.DatabaseOperation;
+
+import java.io.FileInputStream;
+
+import org.junit.Before;
+import roteiros.criacao.RoteiroCriarTicket;
+
+public class testeCriarTicket extends DBTestCase{
+	
+	private FlatXmlDataSet bancoCarregado;
+
+	@Before
+	public void setUp() throws Exception {
+		 	System.setProperty( PropertiesBasedJdbcDatabaseTester.DBUNIT_DRIVER_CLASS, "org.h2.Driver" );
+	        System.setProperty( PropertiesBasedJdbcDatabaseTester.DBUNIT_CONNECTION_URL, "jdbc:h2:file:~/comp3" );
+	        System.setProperty( PropertiesBasedJdbcDatabaseTester.DBUNIT_USERNAME, "sa" );
+	        System.setProperty( PropertiesBasedJdbcDatabaseTester.DBUNIT_PASSWORD, "" );
+	        
+	}
+	
+	public void testCriarCurso() throws Exception{		
+		RoteiroCriarTicket Departamento = new RoteiroCriarTicket();
+		//Departamento.execute();
+		
+		IDataSet dadosSetBanco = getConnection().createDataSet();
+		ITable dadosNoBanco = dadosSetBanco.getTable("departamento");
+		
+		//remove coluna da tabela.
+		ITable filteredTable = DefaultColumnFilter.excludedColumnsTable(dadosNoBanco, new String[]{"id"});
+		
+		IDataSet dadosSetEsperado = new FlatXmlDataSetBuilder().build(new FileInputStream("xml/dataset.xml"));
+		ITable dadosEsperados = dadosSetEsperado.getTable("departamento");
+		
+		Assertion.assertEquals(dadosEsperados, filteredTable);
+	}
+	
+	public void testQuantidadeRegistroTabela() throws Exception{
+		RoteiroCriarTicket Departamento = new RoteiroCriarTicket();
+		//Departamento.execute("teste", "T");
+		
+		IDataSet dadosSetBanco = getConnection().createDataSet();
+		int rowCount = dadosSetBanco.getTable("departamento").getRowCount();
+		
+		assertEquals(1, rowCount);
+	}
+
+	
+	//Antes de executar o teste.
+	protected DatabaseOperation getSetUpOperation() throws Exception{
+		//RoteiroCriarDepartamento Departamento = new RoteiroCriarDepartamento();
+		//Departamento.execute("teste", "T");
+		return DatabaseOperation.REFRESH;		
+	}
+	
+	//depois  de executar a operação.
+	protected DatabaseOperation getTearDownOperation() throws Exception{
+		return DatabaseOperation.DELETE_ALL;	
+		//return DatabaseOperation.NONE;
+	}
+	
+	@Override
+	protected void setUpDatabaseConfig(DatabaseConfig config){
+		config.setProperty(DatabaseConfig.PROPERTY_DATATYPE_FACTORY, new H2DataTypeFactory());		
+	}
+
+	@Override
+	protected IDataSet getDataSet() throws Exception {
+		bancoCarregado = new FlatXmlDataSetBuilder().build( new FileInputStream("xml/dataset.xml"));
+		return bancoCarregado;
+	}
+
+}
