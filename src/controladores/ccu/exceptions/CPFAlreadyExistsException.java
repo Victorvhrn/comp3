868285bf42dd893791a6301c4468e43a3a855cac
Victor <victorhugo.vhrn@gmail.com>
2015-07-15 02:34:02 -0300

    Commit de Exception que não foi no commit anterior

---
+++ src/controladores/ccu/exceptions/CPFAlreadyExistsException.java
@@ -0,0 +1,25 @@
+package controladores.ccu.exceptions;
+
+
+public class CPFAlreadyExistsException extends Exception {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String cpf;
+	
+	public CPFAlreadyExistsException(String cpf) {
+		super("CPF já cadastrado: "+cpf);
+	}
+	
+	public String getCpf() {
+		return cpf;
+	}
+	
+	public boolean equals(CPFAlreadyExistsException e) {
+		// TODO Auto-generated method stub
+		return getMessage().equals(e.getMessage()) && cpf.equals(e.getCpf());
+	}
+}
+
